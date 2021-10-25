ba5c02645a0ea8aa8a630c2fe2bb257c03a34530
Victor Koenders <gpg@trangar.com>
2021-10-25 09:11:12 +0200

    Cleaned up code, made the dns server listen on the local IP instead of 0.0.0.0, added logging info

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,49 @@
-use std::net::SocketAddr;
-
-use std::{io};
-use std::sync::Arc;
-use std::time::SystemTime;
-use std::collections::HashMap;
-use trust_dns_proto::op::{Message, MessageType, OpCode, ResponseCode};
-use trust_dns_proto::serialize::binary::{BinDecoder, BinDecodable, BinEncodable, BinEncoder};
-use trust_dns_client::udp::UdpClientStream;
-use trust_dns_client::client::{AsyncClient, ClientHandle};
-use trust_dns_proto::rr::{DNSClass, RecordType, Record, Name};
-use trust_dns_server::authority::{MessageRequest};
-use trust_dns_client::op::{LowerQuery};
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use tokio::net::UdpSocket;
-use tokio::sync::RwLock;
-use tokio::time::Duration;
-use std::ops::Add;
+use std::{
+    collections::HashMap,
+    fs::File,
+    io,
+    io::{BufRead, BufReader},
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    ops::Add,
+    sync::Arc,
+    time::SystemTime,
+};
+use tokio::{net::UdpSocket, sync::RwLock, time::Duration};
+use trust_dns_client::{
+    client::{AsyncClient, ClientHandle},
+    op::LowerQuery,
+    udp::UdpClientStream,
+};
+use trust_dns_proto::{
+    op::{Message, MessageType, OpCode, ResponseCode},
+    rr::{DNSClass, Name, Record, RecordType},
+    serialize::binary::{BinDecodable, BinDecoder, BinEncodable, BinEncoder},
+};
+use trust_dns_server::authority::MessageRequest;
 
 #[derive(Debug, Clone)]
 struct Answer {
     expires: SystemTime,
-    records: Vec<Record>
+    records: Vec<Record>,
 }
 
 async fn recurse(query: &LowerQuery) -> Option<Vec<Record>> {
-    let stream = UdpClientStream::<UdpSocket>::new(([8,8,8,8], 53).into());
+    let stream = UdpClientStream::<UdpSocket>::new(([8, 8, 8, 8], 53).into());
     let (mut client, dns_background) = AsyncClient::connect(stream).await.unwrap();
 
     tokio::spawn(dns_background);
 
     // Create a query future
     let name: Name = query.name().into();
-    let response = client.query(name, DNSClass::IN, RecordType::A).await.unwrap();
+    let response = client
+        .query(name, DNSClass::IN, RecordType::A)
+        .await
+        .unwrap();
 
     // validate it's what we expected
     let answers = response.answers().to_owned();
 
-    return Some(answers);
+    Some(answers)
 }
 
 fn parse_denylist() -> Option<HashMap<String, bool>> {
@@ -45,23 +51,47 @@ fn parse_denylist() -> Option<HashMap<String, bool>> {
     let reader = BufReader::new(file);
     let mut deny_entries = HashMap::<String, bool>::new();
 
-    for line in reader.lines() {
-        if let Ok(domain) = line{
-            deny_entries.insert(domain.trim_start_matches("||").trim_end_matches("^").to_string(), true);
-            // deny_entries.push(domain.trim_start_matches("||").trim_end_matches("^").to_string());
-        }
+    for domain in reader.lines().flatten() {
+        deny_entries.insert(
+            domain
+                .trim_start_matches("||")
+                .trim_end_matches('^')
+                .to_string(),
+            true,
+        );
     }
 
-    return Some(deny_entries);
+    Some(deny_entries)
 }
 
+fn query_into_readable_name(label: &[LowerQuery]) -> Vec<String> {
+    label
+        .iter()
+        .map(|q| {
+            let name = Name::from(q.name());
+            name.iter().fold(String::new(), |mut str, label| {
+                if !str.is_empty() {
+                    str += "."
+                }
+                str + std::str::from_utf8(label).unwrap_or("[invalid_utf8]")
+            })
+        })
+        .collect()
+}
 
-
-async fn handle_request(socket: Arc<UdpSocket>, src: SocketAddr, partial_buf: Vec<u8>,
-                        deny_entries: Arc<HashMap<String, bool>>,
-                        query_cache: Arc<RwLock<HashMap<(String, RecordType), Answer>>>) {
+async fn handle_request(
+    socket: Arc<UdpSocket>,
+    src: SocketAddr,
+    partial_buf: Vec<u8>,
+    deny_entries: Arc<HashMap<String, bool>>,
+    query_cache: Arc<RwLock<HashMap<(String, RecordType), Answer>>>,
+) {
     let mut decoder = BinDecoder::new(&partial_buf);
     let request = MessageRequest::read(&mut decoder).unwrap();
+    log::info!(
+        "Incoming request for {:?}",
+        query_into_readable_name(request.queries())
+    );
 
     let mut message = Message::new();
     message
@@ -75,29 +105,37 @@ async fn handle_request(socket: Arc<UdpSocket>, src: SocketAddr, partial_buf: Ve
         .set_checking_disabled(false);
 
     let query = request.queries()[0].to_owned();
-    let domain = query.name().to_string().trim_end_matches(".").to_string();
+    let domain = query.name().to_string().trim_end_matches('.').to_string();
     if deny_entries.contains_key(&domain) {
-        message.set_response_code(ResponseCode::NXDomain)
-               .set_authoritative(true);
-    } else  {
+        message
+            .set_response_code(ResponseCode::NXDomain)
+            .set_authoritative(true);
+    } else {
         let maybe_answers = {
             let read_lock = query_cache.read().await;
-            read_lock.get(&(domain.clone(), query.query_type())).cloned()
+            read_lock
+                .get(&(domain.clone(), query.query_type()))
+                .cloned()
         };
         let answers = if let Some(answer) = maybe_answers {
             answer.records
         } else {
             let server_answers = recurse(&query).await.unwrap();
             let mut write_cache = query_cache.write().await;
-            write_cache.insert((domain.clone(), query.query_type()),
-                               Answer {
-                                   expires: SystemTime::now()
-                                       .add(Duration::from_secs(server_answers[0].ttl() as u64)),
-                                   records: server_answers.clone()});
+            write_cache.insert(
+                (domain.clone(), query.query_type()),
+                Answer {
+                    expires: SystemTime::now().add(Duration::from_secs(
+                        server_answers.get(0).map(|a| a.ttl() as u64).unwrap_or(0),
+                    )),
+                    records: server_answers.clone(),
+                },
+            );
             server_answers
         };
 
-        message.add_query(request.queries()[0].original().to_owned())
+        message
+            .add_query(request.queries()[0].original().to_owned())
             .add_answers(answers)
             .set_response_code(ResponseCode::NoError);
     }
@@ -115,16 +153,22 @@ async fn handle_request(socket: Arc<UdpSocket>, src: SocketAddr, partial_buf: Ve
 async fn cache_invalidator(query_cache: Arc<RwLock<HashMap<(String, RecordType), Answer>>>) {
     loop {
         let now = SystemTime::now();
-        query_cache.write().await.retain(|_, answer| answer.expires > now);
-        tokio::time::sleep(Duration::from_secs(1*60)).await;
+        query_cache
+            .write()
+            .await
+            .retain(|_, answer| answer.expires > now);
+        tokio::time::sleep(Duration::from_secs(60)).await;
     }
 }
 
-
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let socket = Arc::new(UdpSocket::bind("0.0.0.0:53").await?);
-    let query_cache: Arc<RwLock<HashMap<(String, RecordType), Answer>>> = Arc::new(RwLock::new(HashMap::new()));
+    let _ = dotenv::dotenv();
+    pretty_env_logger::init();
+    let socket = Arc::new(UdpSocket::bind(get_desired_bind_addr()).await?);
+    log::info!("Listening on {:?}", socket.local_addr().unwrap());
+    let query_cache: Arc<RwLock<HashMap<(String, RecordType), Answer>>> =
+        Arc::new(RwLock::new(HashMap::new()));
 
     let mut buf = [0u8; 1024];
     let deny_entries = Arc::new(parse_denylist().unwrap());
@@ -136,7 +180,7 @@ async fn main() -> io::Result<()> {
 
     loop {
         let (len, addr) = socket.recv_from(&mut buf).await?;
-        let partial_buf = (&mut buf[..len]).to_vec();
+        let partial_buf = (&buf[..len]).to_vec();
         let sock = socket.clone();
         let deny = deny_entries.clone();
         let cache = query_cache.clone();
@@ -147,3 +191,23 @@ async fn main() -> io::Result<()> {
     }
 }
 
+fn get_desired_bind_addr() -> SocketAddr {
+    let addr = if let Some(addr) = std::env::var("HOST")
+        .ok()
+        .and_then(|host| host.parse().ok())
+    {
+        addr
+    } else if let Ok(addr) = local_ip_address::local_ip() {
+        addr
+    } else {
+        IpAddr::V4(Ipv4Addr::LOCALHOST)
+    };
+
+    let port = if let Some(port) = std::env::var("PORT").ok().and_then(|p| p.parse().ok()) {
+        port
+    } else {
+        53u16
+    };
+
+    (addr, port).into()
+}
